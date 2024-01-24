f7ac30a164ec47b9f5a90208012ecc718e0ee5bb
georg <georg@DESKTOP-5G0D5DM>
2024-01-24 18:18:02 +0000

    Add project files.

---
+++ SupportTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SupportTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
