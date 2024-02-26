69af11815b32ae8b2b64ef7f432e29d858e11674
George Share <georgeshare2@gmail.com>
2024-02-26 17:59:49 +0000

    Added DB models

---
+++ SupportTickets/Models/Database/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupportTickets.Models.Database
+{
+    public class Ticket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string Assignee { get; set; } = string.Empty;
+
+        public virtual User User { get; set; }
+    }
+}
