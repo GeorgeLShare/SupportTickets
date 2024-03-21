2e5acc3312e728859040186a010e2eba9da6539f
George Share <georgeshare2@gmail.com>
2024-03-21 14:48:47 +0000

    Created an Account controller and login pages

---
+++ SupportTickets/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SupportTickets.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
